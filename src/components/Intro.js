e09e55a612c4e943508017f6c595587d8da0f0bc
HA24RSH <harshvardhangadagade24555@gmail.com>
2025-04-29 19:11:49 +0530

    about me scroll fixed

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@ import '../styles/Intro.css'
 
 const Intro = () => {
     return (
-        <div className='intro'>
+        <div id='intro' className='intro'>
             <div className='intro-image'>
                 <img src='https://media.licdn.com/dms/image/v2/D4D03AQE5DVR6GBOS-g/profile-displayphoto-shrink_400_400/B4DZZ90hNIGgAg-/0/1745867642094?e=1751500800&v=beta&t=YCOtvU9m8JIESk2pBoM7IGYL9Hixnpu0RIpPvpUbdRM'
                     alt='Profile Pic' />
